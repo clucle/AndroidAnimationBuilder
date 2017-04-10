9091f3fc723f390df633ec5b07b1a1943b83b64b
clucle <wjdenwls123@gmail.com>
2017-04-10 20:35:03 +0900

    feat: update cuty screen rotate

--- Mainactivity/app/src/main/java/shakesplash/com/shake/MainActivity.java
+++ Mainactivity/app/src/main/java/shakesplash/com/shake/MainActivity.java
@@ -1,29 +1,34 @@
 package shakesplash.com.shake;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private Animation shake;
 
     private Button btnShake;
+    private ConstraintLayout customLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        customLayout = (ConstraintLayout) findViewById(R.id.shake_layout);
+
         shake = AnimationUtils.loadAnimation(this, R.anim.shake);
 
-        btnShake = (Button)findViewById(R.id.btn_shake);
+        btnShake = (Button) findViewById(R.id.btn_shake);
         btnShake.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnShake.startAnimation(shake);
+                customLayout.startAnimation(shake);
             }
         });
     }

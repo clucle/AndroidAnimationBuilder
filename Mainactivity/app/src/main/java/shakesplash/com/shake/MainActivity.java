cff59d511d50495d8267ab0d9c4c408c6b4ab5c8
clucle <wjdenwls123@gmail.com>
2017-04-10 20:28:11 +0900

    feat: update btn cuty rotate

--- Mainactivity/app/src/main/java/shakesplash/com/shake/MainActivity.java
+++ Mainactivity/app/src/main/java/shakesplash/com/shake/MainActivity.java
@@ -2,12 +2,30 @@ package shakesplash.com.shake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Animation shake;
+
+    private Button btnShake;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+        btnShake = (Button)findViewById(R.id.btn_shake);
+        btnShake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnShake.startAnimation(shake);
+            }
+        });
     }
+
 }
